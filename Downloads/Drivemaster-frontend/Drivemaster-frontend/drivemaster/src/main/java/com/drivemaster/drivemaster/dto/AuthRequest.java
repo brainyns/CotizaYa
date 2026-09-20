a9155b816ba449acf5a2c92f94f0d4eb161635f6
Brainy Blanco <brainyblanco19@gmail.com>
2026-09-20 18:05:35 -0500

    Eliminar carpeta Downloads

--- Downloads/Drivemaster-frontend/Drivemaster-frontend/drivemaster/src/main/java/com/drivemaster/drivemaster/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.drivemaster.drivemaster.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String correo;
-    private String password;
-}
--- Downloads/app/app/src/main/java/cotizaciones/app/shared/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cotizaciones.app.shared;
-
-public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(Long id) {
-        super("Cliente no encontrado: " + id);
-    }
-}
